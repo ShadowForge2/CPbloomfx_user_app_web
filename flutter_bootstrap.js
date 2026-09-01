cc9e2146b38a0f9d4d97edcb5863e8e27cdda4e6
ShadowForge2 <ShadowForge2@users.noreply.github.com>
2026-09-01 20:14:58 +0100

    Deploy user+admin web: MaxelPay-first deposit flow (checkoutUrl redirect, 'Having a problem?' report, auto-approve, issue_reported admin review)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "449309049" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4156014506" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
